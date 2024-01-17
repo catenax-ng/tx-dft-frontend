1122d1fc178931086cdbfcf382d3d3099af83af3
ajithsimons <ajithkumar.s@t-systems.com>
2024-01-17 13:40:14 +0530

    Chore: Code optimised, sonar cloud fixes, resuable components used

--- src/pages/consumer/ConsumeData.tsx
+++ src/pages/consumer/ConsumeData.tsx
@@ -18,82 +18,37 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
-import { Autocomplete, Box, Grid, LinearProgress } from '@mui/material';
-import {
-  DataGrid,
-  GridColDef,
-  GridSelectionModel,
-  GridToolbar,
-  GridValidRowModel,
-  GridValueGetterParams,
-} from '@mui/x-data-grid';
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogHeader,
-  Input,
-  LoadingButton,
-  SelectList,
-  Typography,
-} from 'cx-portal-shared-components';
+import { Box } from '@mui/material';
+import { GridColDef, GridSelectionModel, GridValidRowModel, GridValueGetterParams } from '@mui/x-data-grid';
+import { Button, Dialog, DialogActions, DialogContent, DialogHeader, Typography } from 'cx-portal-shared-components';
 import saveAs from 'file-saver';
-import { debounce, isEmpty, isEqual } from 'lodash';
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import { isEmpty, isEqual } from 'lodash';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { v4 as uuid } from 'uuid';
 
+import ConsumeDataFilter from '../../components/ConsumeDataFilter';
 import ConfirmTermsDialog from '../../components/dialogs/ConfirmTermsDialog';
 import OfferDetailsDialog from '../../components/dialogs/OfferDetailsDialog';
-import NoDataPlaceholder from '../../components/NoDataPlaceholder';
 import Permissions from '../../components/Permissions';
+import DataTable from '../../components/table/DataTable';
 import {
   setContractOffers,
-  setFfilterCompanyOptionsLoading,
   setFilterCompanyOptions,
   setFilterConnectors,
   setFilterProviderUrl,
   setFilterSelectedBPN,
   setFilterSelectedConnector,
   setIsMultipleContractSubscription,
-  setOffersLoading,
-  setSearchFilterByType,
   setSelectedFilterCompanyOption,
   setSelectedOffer,
   setSelectedOffersList,
   setSelectionModel,
 } from '../../features/consumer/slice';
-import {
-  IConnectorResponse,
-  IConsumerDataOffers,
-  ILegalEntityContent,
-  IntConnectorItem,
-  IntOption,
-} from '../../features/consumer/types';
+import { IConsumerDataOffers } from '../../features/consumer/types';
 import { setSnackbarMessage } from '../../features/notifiication/slice';
 import { useAppDispatch, useAppSelector } from '../../features/store';
 import { handleBlankCellValues } from '../../helpers/ConsumerOfferHelper';
 import ConsumerService from '../../services/ConsumerService';
-import { MAX_CONTRACTS_AGREEMENTS } from '../../utils/constants';
-
-const ITEMS: IntConnectorItem[] = [
-  {
-    id: 1,
-    title: 'Company Name',
-    value: 'company',
-  },
-  {
-    id: 2,
-    title: 'Business Partner Number',
-    value: 'bpn',
-  },
-  {
-    id: 3,
-    title: 'Connector URL',
-    value: 'url',
-  },
-];
 
 export default function ConsumeData() {
   const {
@@ -102,23 +57,11 @@ export default function ConsumeData() {
     selectedOffer,
     selectedOffersList,
     isMultipleContractSubscription,
-    searchFilterByType,
-    filterProviderUrl,
-    filterCompanyOptions,
-    filterCompanyOptionsLoading,
-    filterConnectors,
-    filterSelectedConnector,
-    filterSelectedBPN,
     selectionModel,
   } = useAppSelector(state => state.consumerSlice);
   const [isOpenOfferDialog, setIsOpenOfferDialog] = useState<boolean>(false);
   const [isOpenOfferConfirmDialog, setIsOpenOfferConfirmDialog] = useState<boolean>(false);
-  const [pageSize, setPageSize] = useState<number>(10);
-  const [searchOpen, setSearchOpen] = useState(false);
-  const [conKey, setConKey] = useState(uuid());
-  const [bpnError, setbpnError] = useState(false);
-  const [offerSubLoading, setIsOfferSubLoading] = useState(false);
-
+  const [offerSubLoading, setOfferSubLoading] = useState(false);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
 
@@ -170,7 +113,6 @@ export default function ConsumeData() {
       });
       payload = {
         connectorId: selectedOffersList[0].connectorId,
-        providerUrl: searchFilterByType.value === 'url' ? filterProviderUrl : filterSelectedConnector.value,
         offers: offersList,
         policies: selectedOffersList[0].usagePolicies,
       };
@@ -183,7 +125,6 @@ export default function ConsumeData() {
       });
       payload = {
         connectorId: connectorId,
-        providerUrl: searchFilterByType.value === 'url' ? filterProviderUrl : filterSelectedConnector.value,
         offers: offersList,
         policies: usagePolicies,
       };
@@ -192,7 +133,7 @@ export default function ConsumeData() {
   };
 
   const handleConfirmTermDialog = async () => {
-    setIsOfferSubLoading(true);
+    setOfferSubLoading(true);
     await ConsumerService.getInstance()
       .subscribeToOffers(preparePayload())
       .then(response => {
@@ -213,7 +154,7 @@ export default function ConsumeData() {
         }
       })
       .catch(error => console.log('err', error))
-      .finally(() => setIsOfferSubLoading(false));
+      .finally(() => setOfferSubLoading(false));
   };
 
   const onRowClick = (params: any) => {
@@ -221,40 +162,9 @@ export default function ConsumeData() {
     toggleDialog(true);
   };
 
-  const fetchConsumerDataOffers = async () => {
-    try {
-      let providerUrl = '';
-      if (searchFilterByType.value === 'company' || searchFilterByType.value === 'bpn') {
-        providerUrl = filterSelectedConnector.value;
-      } else {
-        providerUrl = filterProviderUrl;
-      }
-      if (providerUrl == '' || providerUrl == null) {
-        return true;
-      }
-      dispatch(setOffersLoading(true));
-      const response = await ConsumerService.getInstance().fetchConsumerDataOffers({
-        providerUrl: providerUrl,
-        offset: 0,
-        maxLimit: MAX_CONTRACTS_AGREEMENTS,
-      });
-      dispatch(setContractOffers(response.data));
-      dispatch(setOffersLoading(false));
-    } catch (error) {
-      dispatch(setContractOffers([]));
-      dispatch(setOffersLoading(false));
-    }
-  };
-
-  // enter key fetch data
-  const handleKeypress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (['Enter', 'NumpadEnter'].includes(e.key)) {
-      await fetchConsumerDataOffers();
-    }
-  };
-  const [dialogOpen, setdialogOpen] = useState<boolean>(false);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
   const showAddDialog = () => {
-    setdialogOpen(prev => !prev);
+    setDialogOpen(prev => !prev);
   };
 
   const checkoutSelectedOffers = () => {
@@ -282,78 +192,6 @@ export default function ConsumeData() {
     }
   };
 
-  // get company name oninput change
-  const onChangeSearchInputValue = async (params: string) => {
-    const searchStr = params.toLowerCase();
-    if (searchStr.length > 2) {
-      if (open) setSearchOpen(true);
-      dispatch(setFilterCompanyOptions([]));
-      dispatch(setFilterSelectedConnector(null));
-      dispatch(setFfilterCompanyOptionsLoading(true));
-      const res: [] = await ConsumerService.getInstance().searchLegalEntities(searchStr);
-      dispatch(setFfilterCompanyOptionsLoading(false));
-      if (res.length > 0) {
-        const filterContent = res.map((item: ILegalEntityContent, index) => {
-          return {
-            _id: index,
-            bpn: item.bpn,
-            value: item.name,
-          };
-        });
-        dispatch(setFilterCompanyOptions(filterContent));
-      }
-    } else {
-      setSearchOpen(false);
-      dispatch(setFilterCompanyOptions([]));
-    }
-  };
-
-  // on change search type filter option
-  const handleSearchTypeChange = (value: IntConnectorItem) => {
-    dispatch(setSearchFilterByType(value));
-    dispatch(setSelectedFilterCompanyOption(null));
-    dispatch(setFilterProviderUrl(''));
-    dispatch(setFilterSelectedBPN(''));
-    dispatch(setFilterConnectors([]));
-    dispatch(setFilterSelectedConnector(null));
-    setConKey(uuid());
-  };
-
-  const getConnectorByBPN = async (bpn: string) => {
-    const payload = [];
-    payload.push(bpn);
-    dispatch(setFilterSelectedConnector(null));
-    dispatch(setFilterConnectors([]));
-    try {
-      const res = await ConsumerService.getInstance().searchConnectoByBPN(payload);
-      if (res.length) {
-        const resC: IConnectorResponse[] = res;
-        const connector = resC[0];
-        const optionConnectors = connector.connectorEndpoint.map((item, index) => {
-          return {
-            id: index,
-            value: item,
-            title: item,
-          };
-        });
-        dispatch(setFilterConnectors(optionConnectors));
-      } else {
-        dispatch(setSnackbarMessage({ message: 'alerts.noConnector', type: 'error' }));
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  // on option selected of company dropdown
-  const onCompanyOptionChange = async (value: IntOption | string) => {
-    const payload = value as IntOption;
-    dispatch(setSelectedFilterCompanyOption(payload));
-    if (payload !== null) {
-      await getConnectorByBPN(payload.bpn);
-    }
-  };
-
   const handleSelectionModel = (newSelectionModel: GridSelectionModel) => {
     const selectedIDs = new Set(newSelectionModel);
     const selectedRowData = contractOffers.filter((row: GridValidRowModel) => selectedIDs.has(row.id));
@@ -366,7 +204,6 @@ export default function ConsumeData() {
     dispatch(setSelectedOffer(null));
     dispatch(setSelectedOffersList([]));
     dispatch(setSelectionModel([]));
-    dispatch(setSearchFilterByType(ITEMS[0]));
     dispatch(setSelectedFilterCompanyOption(null));
     dispatch(setFilterCompanyOptions([]));
     dispatch(setFilterProviderUrl(''));
@@ -380,22 +217,6 @@ export default function ConsumeData() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleBPNchange = (e: ChangeEvent<HTMLInputElement>) => {
-    const regex = /[a-zA-Z0-9]$/;
-    const { value } = e.target;
-    if (value === '' || regex.test(value)) {
-      dispatch(setFilterSelectedBPN(value));
-      if (value.length == 16) {
-        getConnectorByBPN(value);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (filterSelectedBPN.length == 16 || filterSelectedBPN.length == 0) setbpnError(false);
-    else setbpnError(true);
-  }, [filterSelectedBPN]);
-
   return (
     <>
       <Typography variant="h3" mb={1}>
@@ -404,131 +225,7 @@ export default function ConsumeData() {
       <Typography variant="body1" mb={4} maxWidth={900}>
         {t('content.consumeData.description')}
       </Typography>
-      <Grid container spacing={2} alignItems="end">
-        <Grid item xs={3}>
-          <SelectList
-            keyTitle="title"
-            label={t('content.consumeData.selectType')}
-            placeholder={t('content.consumeData.selectType')}
-            defaultValue={searchFilterByType}
-            items={ITEMS}
-            onChangeItem={e => handleSearchTypeChange(e)}
-            disableClearable={true}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          {searchFilterByType.value === 'url' ? (
-            <Input
-              value={filterProviderUrl}
-              type="url"
-              onChange={e => dispatch(setFilterProviderUrl(e.target.value))}
-              onKeyDown={handleKeypress}
-              fullWidth
-              size="small"
-              label={t('content.consumeData.enterURL')}
-              placeholder={t('content.consumeData.enterURL')}
-            />
-          ) : (
-            <Grid container spacing={1} alignItems="flex-end">
-              <Grid item xs={7}>
-                {searchFilterByType.value === 'bpn' ? (
-                  <Input
-                    value={filterSelectedBPN}
-                    type="text"
-                    fullWidth
-                    size="small"
-                    label={t('content.consumeData.enterBPN')}
-                    placeholder={t('content.consumeData.enterBPN')}
-                    inputProps={{ maxLength: 16 }}
-                    error={bpnError}
-                    onChange={handleBPNchange}
-                    helperText={t('alerts.bpnValidation')}
-                  />
-                ) : (
-                  <Autocomplete
-                    open={searchOpen}
-                    options={filterCompanyOptions}
-                    includeInputInList
-                    loading={filterCompanyOptionsLoading}
-                    onChange={async (event, value: any) => {
-                      await onCompanyOptionChange(value);
-                      setConKey(uuid());
-                    }}
-                    onInputChange={debounce(async (event, newInputValue) => {
-                      await onChangeSearchInputValue(newInputValue);
-                    })}
-                    onSelect={() => setSearchOpen(false)}
-                    onBlur={() => setSearchOpen(false)}
-                    onClose={() => setSearchOpen(false)}
-                    isOptionEqualToValue={(option, value) => option.value === value.value}
-                    getOptionLabel={option => {
-                      return typeof option === 'string' ? option : `${option.value}`;
-                    }}
-                    noOptionsText={t('content.consumeData.noCompany')}
-                    renderInput={params => (
-                      <Input
-                        {...params}
-                        label={t('content.consumeData.searchCompany')}
-                        placeholder={t('content.consumeData.searchPlaceholder')}
-                        fullWidth
-                      />
-                    )}
-                    renderOption={(props, option: any) => (
-                      <Box
-                        component="li"
-                        {...props}
-                        key={option.bpn}
-                        sx={{
-                          display: 'flex',
-                          flexDirection: 'column',
-                          alignItems: 'initial!important',
-                          justifyContent: 'initial',
-                        }}
-                      >
-                        <Typography variant="subtitle1">{option.value}</Typography>
-                        <Typography variant="subtitle2">{option.bpn}</Typography>
-                      </Box>
-                    )}
-                    sx={{
-                      '& .MuiFilledInput-root': {
-                        pt: '0px!important',
-                        minHeight: '55px',
-                      },
-                    }}
-                  />
-                )}
-              </Grid>
-              <Grid item xs={5}>
-                <SelectList
-                  key={conKey}
-                  disabled={!filterConnectors.length}
-                  keyTitle="title"
-                  label={t('content.consumeData.selectConnectors')}
-                  placeholder={t('content.consumeData.selectConnectors')}
-                  noOptionsText={t('content.consumeData.noConnectors')}
-                  defaultValue={filterSelectedConnector}
-                  onChangeItem={e => dispatch(setFilterSelectedConnector(e))}
-                  items={filterConnectors}
-                />
-              </Grid>
-            </Grid>
-          )}
-        </Grid>
-        <Grid item>
-          <Permissions values={['consumer_search_connectors']}>
-            <LoadingButton
-              color="primary"
-              variant="contained"
-              disabled={isEmpty(filterSelectedConnector) && isEmpty(filterProviderUrl)}
-              label={t('button.search')}
-              loadIndicator={t('content.common.loading')}
-              onButtonClick={fetchConsumerDataOffers}
-              loading={offersLoading}
-              sx={{ ml: 3 }}
-            />
-          </Permissions>
-        </Grid>
-      </Grid>
+      <ConsumeDataFilter />
       <Box display="flex" justifyContent="flex-end" my={3}>
         <Permissions values={['consumer_subscribe_download_data_offers']}>
           <Button
@@ -543,49 +240,14 @@ export default function ConsumeData() {
       </Box>
       <Permissions values={['consumer_view_contract_offers']}>
         <Box sx={{ height: 'auto', overflow: 'auto', width: '100%' }}>
-          <DataGrid
-            autoHeight={true}
-            getRowId={row => row.id}
-            rows={contractOffers}
-            onRowClick={onRowClick}
+          <DataTable
+            data={contractOffers}
             columns={columns}
-            loading={offersLoading}
-            checkboxSelection
-            pagination
-            pageSize={pageSize}
-            onPageSizeChange={setPageSize}
-            rowsPerPageOptions={[10, 25, 50, 100]}
-            onSelectionModelChange={newSelectionModel => handleSelectionModel(newSelectionModel)}
+            isFetching={offersLoading}
+            checkboxSelection={true}
+            onRowClick={() => onRowClick}
+            handleSelectionModel={newSelectionModel => handleSelectionModel(newSelectionModel)}
             selectionModel={selectionModel}
-            components={{
-              Toolbar: GridToolbar,
-              LoadingOverlay: LinearProgress,
-              NoRowsOverlay: () => NoDataPlaceholder('content.common.noData'),
-              NoResultsOverlay: () => NoDataPlaceholder('content.common.noResults'),
-            }}
-            componentsProps={{
-              toolbar: {
-                showQuickFilter: true,
-                quickFilterProps: { debounceMs: 500 },
-                printOptions: { disableToolbarButton: true },
-                csvOptions: { disableToolbarButton: true },
-              },
-            }}
-            disableColumnMenu
-            disableColumnSelector
-            disableDensitySelector
-            disableSelectionOnClick
-            sx={{
-              '& .MuiDataGrid-columnHeaderTitle': {
-                textOverflow: 'clip',
-                whiteSpace: 'break-spaces',
-                lineHeight: 1.5,
-                textAlign: 'center',
-              },
-              '& .MuiDataGrid-columnHeaderCheckbox': {
-                height: 'auto !important',
-              },
-            }}
           />
         </Box>
       </Permissions>
